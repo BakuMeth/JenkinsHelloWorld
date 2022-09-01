ac41f178e70c4b82ba06f09ece727db719290285
baku_meth <jonelgoyal@gmail.com>
2022-09-01 13:28:38 +0800

    Adding new files

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+package com.demo.jenkins;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("-----***** Hello World & Welcome To Jenkins Training *****-----");
+    }
+}
